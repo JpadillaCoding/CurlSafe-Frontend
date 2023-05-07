70f706ebb865a522b0540bd5dc8ebd5326502e76
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-07 15:57:01 -0500

    feat: created navigation starting with homescreen

--- CurlSafe/App.js
+++ CurlSafe/App.js
@@ -3,14 +3,22 @@ import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>homepage</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen}/>
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
