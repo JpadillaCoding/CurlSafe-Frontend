6a855e0ee17d3f81e3e68ab837fa9d28424d8336
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-07 15:17:17 -0500

    feat: setup initial redux files

---
+++ CurlSafe/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>homepage</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ CurlSafe/slices/navSlices.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  origin: null, //change
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+  },
+});
+
+export const { setOrigin } = navSlice.actions;
+
+export const selectOrigin = (state) => {
+  state.nav.origin;
+};
+
+export default navSlice.reducer;
