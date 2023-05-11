77c63c97ccec2d7e037679b249e027da82e4c016
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-10 22:00:01 -0500

    feat: setup states for camera use

--- CurlSafe/screens/CameraScreen.js
+++ CurlSafe/screens/CameraScreen.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-
+import { Camera } from "expo-camera";
 
 const CameraScreen = () => {
-    return (
-        <View>
-
-        </View>
-    )
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null)
+  return (
+    <View>
+      <Camera 
+      type={type}
+      flashMode={flash}
+      ref={cameraRef}
+      >
+        
+      </Camera>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({});
 
-})
-
-export default CameraScreen
+export default CameraScreen;
