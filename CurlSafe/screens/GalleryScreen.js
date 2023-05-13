fa2cab413a87cc4666d0b2afcfba99e3a571768d
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-13 11:55:38 -0500

    style: preview settings set up- aspect ratio and images only

--- CurlSafe/screens/GalleryScreen.js
+++ CurlSafe/screens/GalleryScreen.js
@@ -9,9 +9,9 @@ const GalleryScreen = () => {
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [3, 4],
       quality: 1,
     });
 
