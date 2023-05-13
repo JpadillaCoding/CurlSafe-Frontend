cc2c9163c2bb0333ff4e881a13dd63f441e2252a
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-12 20:47:05 -0500

    feat: image picker button and function working

--- CurlSafe/screens/GalleryScreen.js
+++ CurlSafe/screens/GalleryScreen.js
@@ -1,29 +1,34 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import * as ImagePicker from 'expo-image-picker'
+import * as ImagePicker from "expo-image-picker";
+import Button from "../components/Button";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
 
 const GalleryScreen = () => {
-    const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null);
 
-    const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            aspect: [4, 3],
-            quality: 1,
-        });
-        console.log(result)
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
     }
+  };
 
-    return (
-        <View>
-
-        </View>
-    )
+  return (
+    <View>
+      <Button title={"Choose an image"} onPress={pickImage} color={"#f8b71c"} />
+      {image && (<Image source={{ uri: image }} style={{ width: 200, height: 200 }} icon={{faImage}}/>)}
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({});
 
-})
-
-export default GalleryScreen
+export default GalleryScreen;
