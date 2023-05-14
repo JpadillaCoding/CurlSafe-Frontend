fd1c260d6f8ea2e010d7ffc6706e3f6fb43801ce
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-14 18:41:27 -0500

    feat: api request for ingredients

--- CurlSafe/screens/AnalysisScreen.js
+++ CurlSafe/screens/AnalysisScreen.js
@@ -1,49 +1,74 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native/";
 import { useSelector } from "react-redux";
+import axios from "axios";
+
+const DATA = [
+  {
+    id: "25",
+    title: "sulfate",
+    description: "insert text here1",
+  },
+  {
+    id: "26",
+    title: "Silicone",
+    description: "insert text here2",
+  },
+  {
+    id: "27",
+    title: "silicones(water-soluble)",
+    description: "insert text here3",
+  },
+  {
+    id: "28",
+    title: "short-chain-alcohol",
+    description: "insert text here4",
+  },
+  {
+    id: "29",
+    title: "parabens",
+    description: "insert text here5",
+  },
+  {
+    id: "30",
+    title: "formaldehyde",
+    description: "insert text here6",
+  },
+  {
+    id: "31",
+    title: "soap",
+    description: "insert text here7",
+  },
+  {
+    id: "32",
+    title: "wax",
+    description: "insert text here8",
+  },
+];
 
 const AnalysisScreen = () => {
   const results = useSelector((state) => state.results.value);
-
-    const DATA = [
-        {
-            id: '25',
-            title:'sulfate'
-        },
-        {
-            id:'26',
-            title:'Silicone'
-        },
-        {
-            id:'27',
-            title: 'silicones(water-soluble)'
-        },
-        {
-            id:'28',
-            title:'short-chain-alcohol'
-        },
-        {
-            id:'29',
-            title:'parabens'
-        },
-        {
-            id:'30',
-            title:'formaldehyde'
-        },
-        {
-            id:'31',
-            title:'soap'
-        },
-        {
-            id:'32',
-            title:'wax'
-        }
-    ]
-
+    results[0].split(",")
+  const ingredientDatabase = async () => {
+    try {
+      const response = await axios.get("https://236d-2601-2c4-4600-c3b0-c91e-638c-49d-edce.ngrok-free.app/ingredients");
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  ingredientDatabase();
+  console.log(results);
   return (
-    <View>
-      <Text>yooo</Text>
-    </View>
+    <FlatList
+      data={DATA}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => {
+        <View>
+          <Text>{item.title}</Text>
+        </View>;
+      }}
+    ></FlatList>
   );
 };
 const styles = StyleSheet.create({});
