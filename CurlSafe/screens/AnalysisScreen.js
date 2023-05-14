6bace73e2017e6795fa69348c04ceb4026859307
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-14 14:41:35 -0500

    feat: setup data needed for flatlist

--- CurlSafe/screens/AnalysisScreen.js
+++ CurlSafe/screens/AnalysisScreen.js
@@ -3,16 +3,49 @@ import { View, Text, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 
 const AnalysisScreen = () => {
+  const results = useSelector((state) => state.results.value);
 
+    const DATA = [
+        {
+            id: '25',
+            title:'sulfate'
+        },
+        {
+            id:'26',
+            title:'Silicone'
+        },
+        {
+            id:'27',
+            title: 'silicones(water-soluble)'
+        },
+        {
+            id:'28',
+            title:'short-chain-alcohol'
+        },
+        {
+            id:'29',
+            title:'parabens'
+        },
+        {
+            id:'30',
+            title:'formaldehyde'
+        },
+        {
+            id:'31',
+            title:'soap'
+        },
+        {
+            id:'32',
+            title:'wax'
+        }
+    ]
 
-    return(
-        <View>
-            <Text>yooo</Text>
-        </View>
-    )
-}
-const styles = StyleSheet.create({
-    
-})
+  return (
+    <View>
+      <Text>yooo</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
 
-export default AnalysisScreen
+export default AnalysisScreen;
