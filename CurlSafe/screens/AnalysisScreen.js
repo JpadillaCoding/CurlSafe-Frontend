f0497b47556f43ab31a2fddb5a9af3b903826bf9
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-17 19:52:07 -0500

    bug: analyzer now matches to word by using exact ingredients

--- CurlSafe/screens/AnalysisScreen.js
+++ CurlSafe/screens/AnalysisScreen.js
@@ -11,17 +11,17 @@ const DATA = [
   },
   {
     id: "26",
-    title: "Silicone",
+    title: "silicone",
     description: "insert text here2",
   },
   {
     id: "27",
-    title: "silicones(water-soluble)",
+    title: "siliconesWaterSoluble",
     description: "insert text here3",
   },
   {
     id: "28",
-    title: "short-chain-alcohol",
+    title: "shortChainAlcohol",
     description: "insert text here4",
   },
   {
@@ -47,33 +47,36 @@ const DATA = [
 ];
 
 const AnalysisScreen = () => {
-    const results = useSelector((state) => state.results.value);
-    const resultsArr = results.split(",");
-    const [databaseIngredients, setDatabaseIngredients] = useState(null);
-  
-    useEffect(() => {
-      const ingredientDatabase = async () => {
-        try {
-          const response = await axios.get("https://236d-2601-2c4-4600-c3b0-c91e-638c-49d-edce.ngrok-free.app/ingredients");
-          setDatabaseIngredients(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      ingredientDatabase();
-    }, []);
-  
-    if (databaseIngredients) {
-      for (let i = 0; i < resultsArr.length; i++) {
-        for (let j = 0; j < databaseIngredients.length; j++) {
-          if (resultsArr[i].includes(databaseIngredients[j].name)) {
-            console.log("Substring: " + databaseIngredients[j].name);
-            console.log("Is in analyzed item: " + resultsArr[i]);
-          }
-        }
+  const results = useSelector((state) => state.results.value);
+  const resultsArr = results.split(",");
+  const resultsTrimmed = resultsArr.map(string => string.trim())
+  const [databaseIngredients, setDatabaseIngredients] = useState(null);
+  const matches = []
+
+  useEffect(() => {
+    const ingredientDatabase = async () => {
+      try {
+        const response = await axios.get(
+          "https://236d-2601-2c4-4600-c3b0-c91e-638c-49d-edce.ngrok-free.app/ingredients"
+        );
+        setDatabaseIngredients(response.data);
+      } catch (error) {
+        console.error(error);
       }
-    }
-  console.log('done')
+    };
+    ingredientDatabase();
+  }, []);
+  if(databaseIngredients) {
+      databaseIngredients.forEach((item) => {
+        item.ingredients.forEach((ingredient) => {
+            if(resultsTrimmed.includes(ingredient)) {
+                matches.push({type: item.type, ingredient})
+            }
+        })
+      })
+  }
+  console.log(matches)
+  console.log("done");
   return (
     <FlatList
       data={DATA}
