eb8337922ee11ab5056d3f60e871b12097b8a801
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-27 03:29:51 -0500

    feat: status of fetch defined and set reducer to store

--- CurlSafe/slices/ingredientsSlice.js
+++ CurlSafe/slices/ingredientsSlice.js
@@ -2,24 +2,39 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-    ingredients: [],
-    error: ''
-}
+  ingredients: [],
+  status: "idle",
+  error: "",
+};
 
-export const fetchIngredients = createAsyncThunk('ingredients', () => {
-    return axios.get('https://curl-safe.herokuapp.com/ingredients')
-    .then((response) => console.log(response))
-})
-
-export const resultsSlice = createSlice({
-  name: 'ingredients',
-  initialState,
-  extraReducers: {
-
+export const fetchIngredients = createAsyncThunk("/ingredients", async () => {
+  try {
+    const response = await axios.get(
+      "https://curl-safe.herokuapp.com/ingredients"
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error, "Error fetching");
   }
+});
 
-})
+const resultsSlice = createSlice({
+  name: "ingredients",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchIngredients.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchIngredients.fulfilled, (state) => {
+        state.status = "succeeded";
+        state.ingredients = action.payload;
+      })
+      .addCase(fetchIngredients.rejected, (state) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
 
-export const { setResults } = resultsSlice.actions
-
-export default resultsSlice.reducer
+export const { actions, reducer } = resultsSlice;
--- CurlSafe/store.js
+++ CurlSafe/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import resultsReducer from "./slices/resultsSlice";
+import { reducer as ingredientsReducer } from "./slices/ingredientsSlice"
 
 export const store = configureStore({
     reducer: {
         results: resultsReducer,
+        ingredients: ingredientsReducer
     },
 });
