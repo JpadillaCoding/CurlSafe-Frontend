83af99f3ec01a2f46b4f7be707fced9259b55d10
Jose Padilla <jose.apadilla99@gmail.com>
2023-05-28 00:11:57 -0500

    style: styling for searchbar and buttons

--- CurlSafe/components/SearchBar.js
+++ CurlSafe/components/SearchBar.js
@@ -7,7 +7,7 @@ const SearchBar = ({ /* clicked, */ searchPhrase, setSearchPhrase, setClicked })
     const clicked = true
   return (
     <View style={styles.searchBarContainer}>
-      <View style={clicked ? styles.searchbar_clicked : styles.searchbar_unclicked}>
+      <View style={clicked ? styles.searchBar_clicked : styles.searchBar_unclicked}>
         <FontAwesomeIcon icon={faMagnifyingGlass} color={"black"} size={20} />
         <TextInput
           style={styles.input}
@@ -15,7 +15,7 @@ const SearchBar = ({ /* clicked, */ searchPhrase, setSearchPhrase, setClicked })
           value={searchPhrase}
           onChangeText={setSearchPhrase}
           onFocus={() => {
-            /* setClicked(true); */
+            // setClicked(true); 
           }}
         />
         {clicked && (
@@ -30,7 +30,7 @@ const SearchBar = ({ /* clicked, */ searchPhrase, setSearchPhrase, setClicked })
             title="Cancel"
             onPress={() => {
               Keyboard.dismiss();
-              /* setClicked(false); */
+            // setClicked(false); 
             }}
           ></Button>
         </View>
@@ -39,5 +39,35 @@ const SearchBar = ({ /* clicked, */ searchPhrase, setSearchPhrase, setClicked })
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    searchBarContainer: {
+        margin: 15,
+        justifyContent: "flex-start",
+        alignItems: "center",
+        flexDirection: "row",
+        width: "90%",
+      },
+      searchBar_unclicked: {
+        padding: 10,
+        flexDirection: "row",
+        width: "95%",
+        backgroundColor: "#d9dbda",
+        borderRadius: 15,
+        alignItems: "center",
+      },
+      searchBar_clicked: {
+        padding: 10,
+        flexDirection: "row",
+        width: "80%",
+        backgroundColor: "#d9dbda",
+        borderRadius: 15,
+        alignItems: "center",
+        justifyContent: "space-evenly",
+      },
+      input: {
+        fontSize: 20,
+        marginLeft: 10,
+        width: "90%",
+      },
+});
 export default SearchBar;
